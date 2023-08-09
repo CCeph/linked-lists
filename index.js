22c9aab2626ba9ebea1ea49b015e852e8ca17988
CCeph <yousefelsisyy@gmail.com>
2023-08-08 21:30:14 -0300

    Create draft linkedList prototype

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-function createLinkedList() {}
+const linkedListPrototype = {
+  append(value) {
+    console.log("Add value to end of list");
+  },
+
+  prepend(value) {
+    console.log("Add value to start of list");
+  },
+
+  size: 0,
+
+  head: console.log("Head of list"),
+
+  tail: console.log("Tail of list"),
+
+  at(index) {
+    console.log("Return node at index");
+  },
+
+  pop() {
+    console.log("Remove last node");
+  },
+
+  contains(value) {
+    console.log("Return true if value is in the list");
+  },
+
+  find(value) {
+    console.log("Return the index of the node containing value, or null");
+  },
+
+  toString() {
+    console.log("Print list as a string");
+  },
+};
+
+function createLinkedList(array) {}
 
 function createNode() {}
